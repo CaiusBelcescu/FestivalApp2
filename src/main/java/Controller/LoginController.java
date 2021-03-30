b2781266b64e6bb3fc752dc9e952e64449d4ca6d
GorogSergiu <sergiugorog@yahoo.com>
2021-03-30 21:17:29 +0300

    added Login Page

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,4 +1,24 @@
 package Controller;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private void exit(ActionEvent event)
+    {
+        System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }

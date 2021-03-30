6eaa4c2c5230bec26c0f7106c41973655e57414e
GorogSergiu <sergiugorog@yahoo.com>
2021-03-30 21:17:29 +0300

    added Login Page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 //import services.UserService;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,9 +27,26 @@ public class Main extends Application {
         //UserService.loadUsersFromFile();
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+
+        Scene scene = new Scene(root);
+
         primaryStage.setTitle("FestivalWord");
-        primaryStage.setScene(new Scene(root, 737, 524));
+        primaryStage.setScene(scene);
         //primaryStage.getIcons().add(new Image("rellow.jpg"));
+        primaryStage.initStyle(StageStyle.DECORATED.UNDECORATED);
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event){
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event){
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
         primaryStage.setResizable(false);
         primaryStage.show();
     }

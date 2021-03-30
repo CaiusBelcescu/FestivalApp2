859ff30e15387089e37cf811b142e3cf1bd0d154
CaiusBelcescu <80705095+CaiusBelcescu@users.noreply.github.com>
2021-03-30 18:29:17 +0300

    New controllers added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,27 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+//import services.UserService;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //UserService.loadUsersFromFile();
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+        primaryStage.setTitle("FestivalWord");
+        primaryStage.setScene(new Scene(root, 737, 524));
+        //primaryStage.getIcons().add(new Image("rellow.jpg"));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }

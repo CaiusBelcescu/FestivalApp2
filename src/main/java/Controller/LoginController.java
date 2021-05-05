d7eacfe3194971ae4076cdb8b838827db1387834
GorogSergiu <sergiugorog@yahoo.com>
2021-05-05 17:57:31 +0300

    added Register Page

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -4,12 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    @FXML
+    private Label loginMessageLabel;
 
     @FXML
     private void exit(ActionEvent event)
@@ -20,5 +24,9 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void loginButtonOnAction(ActionEvent event){
+        loginMessageLabel.setText("You try to login");
     }
 }

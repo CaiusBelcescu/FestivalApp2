0f8c27d5899282020376ea54ee2d0c56146f1f89
CaiusBelcescu <80705095+CaiusBelcescu@users.noreply.github.com>
2021-05-15 16:10:12 +0300

    controrele

---
+++ src/main/java/Controller/AddFestController.java
@@ -0,0 +1,37 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddFestController {
+
+    @FXML
+    private TextField FestName;
+
+    @FXML
+    private TextField Place;
+
+    @FXML
+    private TextField Prices;
+
+    @FXML
+    private TextField Description;
+
+    @FXML
+    private Button addFest;
+
+    @FXML
+    private Button cancelFest;
+
+    @FXML
+    void addFestButton() {
+
+    }
+
+    @FXML
+    void cancelFestButton() {
+
+    }
+
+}
